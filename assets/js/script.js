d1704d6486caf345076527cb704c26c640169aca
Kr36pton <jmmesa37@misena.edu.co>
2022-02-13 17:04:39 -0500

    Se agrega la funcion para modificar la posicion vertical de nuestra barra (eje Y) con las teclas arriba y abajo, ademas la posicion se imprime en la consola

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,13 +24,17 @@
         this.board = board;
         this.board.bars.push(this);
         this.kind = "square";
+        this.speed = 10;
     }
     self.Bar.prototype = {
         down: function(){
-
+            this.y += this.speed;
         },
         up: function(){
-
+            this.y -= this.speed;
+        },
+        toString: function(){
+            return "x: "+ this.x +" y: "+ this.y ;
         }
     }
 })();
@@ -60,15 +64,27 @@
         }
     }
 })();
-
-self.addEventListener("load",main);
-
-function main(){
-    console.log("Hola mondongo");
     var board = new Board(800,400);
     var canvas = document.getElementById('canvas');
     var board_view = new BoardView(canvas,board);
     var bar = new Bar(20,100,40,100,board);
     var bar = new Bar(740,100,40,100,board);
     board_view.draw();
+
+document.addEventListener("keydown",function(ev){
+    console.log(ev.keyCode);
+    if(ev.keyCode == 38){
+        bar.up();
+    }
+    else if (ev.keyCode == 40){
+        bar.down();
+    }
+    console.log(""+bar);
+})
+
+self.addEventListener("load",main);
+
+function main(){
+    console.log("Hola mondongo");
+    
 }
