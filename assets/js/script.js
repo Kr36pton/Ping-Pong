047c1a5b05572537c1d18a3fec8995c8a3f6fee8
Kr36pton <jmmesa37@misena.edu.co>
2022-02-13 18:51:06 -0500

    Las barras ahora se mueven visualmente y atravez de la funcion clean no dejan una marca por donde pasan, haciendolas usables, ademas se crea la pelota, pero esta genera que la pagina se congele; este error se corregira en el siguiente commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,6 +16,19 @@
     }
 })();
 (function(){
+    self.Ball = function(x,y,radius,board){
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.speed_x = 0;
+        this.speed_y = 3;
+        this.board = board;
+
+        board.ball = this;
+        this.kind = "circle";
+    }
+})();
+(function(){
     self.Bar = function(x,y,width,height,board){
         this.x = x;
         this.y = y;
@@ -47,20 +60,31 @@
         this.ctx = canvas.getContext("2d");
     }
     self.BoardView.prototype = {
+        clean: function(){
+            this.ctx.clearRect(0,0,this.board.width,this.board.height);
+        },
         draw: function(){
             for (var i = this.board.elements.length -  1; i >= 0; i--){
                 var el = this.board.elements[i];
                 draw(this.ctx,el);
             };
+        },
+        play: function(){
+            this.clean();
+            this.draw();
         }
     }
     function draw(ctx,element){
-        if(element !== null && element.hasOwnProperty("kind")){
-            switch(element.kind){
-                case "square":
-                    ctx.fillRect(element.x,element.y,element.width,element.height);
-                    break;
-            }
+        switch(element.kind){
+            case "square":
+                ctx.fillRect(element.x,element.y,element.width,element.height);
+                break;
+            case "circle":
+                ctx.beginPath();
+                ctx.arc(element.x,element.y,element.radius,0,7);
+                ctx.fill();
+                ctx.closePath();
+                break;
         }
     }
 })();
@@ -68,23 +92,32 @@
     var canvas = document.getElementById('canvas');
     var board_view = new BoardView(canvas,board);
     var bar = new Bar(20,100,40,100,board);
-    var bar = new Bar(740,100,40,100,board);
-    board_view.draw();
+    var bar_2 = new Bar(740,100,40,100,board);
+    var ball = new Ball(400,200,10,board);
 
 document.addEventListener("keydown",function(ev){
-    console.log(ev.keyCode);
+    ev.preventDefault();
     if(ev.keyCode == 38){
         bar.up();
     }
     else if (ev.keyCode == 40){
         bar.down();
     }
+    else if(ev.keyCode === 87){
+        ev.preventDefault();
+        bar_2.up();
+    }
+    else if(ev.keyCode === 83){
+        ev.preventDefault();
+        bar_2.down();
+    }
     console.log(""+bar);
+    console.log(""+bar_2);
 })
 
-self.addEventListener("load",main);
+window.requestAnimationFrame(controller);
 
-function main(){
-    console.log("Hola mondongo");
-    
+function controller(){
+    board_view.play();
+    window.requestAnimationFrame(controller);
 }
